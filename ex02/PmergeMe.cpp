be4121448956aec7ad8773b45eeef173cde1e65b
Bernardo Castelo Branco <bernardo.branco@streambow.com>
2025-02-22 09:57:05 +0000

    Improving Ford algorithm

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -6,17 +6,19 @@
 /*   By: bcastelo <bcastelo@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 11:22:23 by bcastelo          #+#    #+#             */
-/*   Updated: 2025/02/15 12:04:48 by bcastelo         ###   ########.fr       */
+/*   Updated: 2025/02/17 22:51:48 by bcastelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
-void sort_pairs_with_vector(std::vector<unsigned int>::iterator start, std::vector<unsigned int>::iterator end);
+void sort_pairs_with_vector(std::vector<unsigned int>::iterator start, std::vector<unsigned int>::iterator end, unsigned int set_size);
 
 void sort_all_pairs_with_vector(std::vector<unsigned int>& data, std::vector<unsigned int>::iterator start, std::vector<unsigned int>::iterator end);
 
 void merge_insertion_with_vector(std::vector<unsigned int>& numbers, unsigned int nbr);
+
+void insert_to_main_with_vector(std::vector<unsigned int>& main, std::vector<unsigned int>& pend);
 
 int insert_into_vector(int argc, char **argv, std::vector<unsigned int>& numbers);
 
@@ -44,18 +46,18 @@ int sort_with_vector(int argc, char **argv)
     start = get_time_microseconds();
     if (!insert_into_vector(argc, argv, unordered))
         return (-1);
-    sort_pairs_with_vector(unordered.begin(), unordered.end());
+    sort_pairs_with_vector(unordered.begin(), unordered.end(), 1);
+    insert_to_main_with_vector(sorted, unordered);
     end = get_time_microseconds();
     print_unordered(argc, argv);
-    print_vector(unordered, "After : ");
+    print_vector(sorted, "After : ");
     std::cout << "Time to process a range of " << unordered.size() << " elements with std::vector : " << end - start << " us" << std::endl;
     return (0);
 }
 
-void sort_pairs_with_vector(std::vector<unsigned int>::iterator start, std::vector<unsigned int>::iterator end)
+void sort_pairs_with_vector(std::vector<unsigned int>::iterator start, std::vector<unsigned int>::iterator end, unsigned int set_size)
 {
     unsigned int aux;
-    unsigned int set_size = 1;
     unsigned int pair_size = set_size * 2;
     std::vector<unsigned int>::iterator init = start;
     
@@ -72,6 +74,8 @@ void sort_pairs_with_vector(std::vector<unsigned int>::iterator start, std::vect
         }
         init += pair_size;
     }
+    //if (set_size < end - start)
+    //    sort_pairs_with_vector(start, end, set_size * 2);
 }
 
 void sort_all_pairs_with_vector(std::vector<unsigned int>& data, std::vector<unsigned int>::iterator start, std::vector<unsigned int>::iterator end)
@@ -104,6 +108,12 @@ void merge_insertion_with_vector(std::vector<unsigned int>& numbers, unsigned in
     std::vector<unsigned int>::iterator right = numbers.end() - 1;
     std::vector<unsigned int>::iterator mid;
 
+    if (!numbers.size())
+    {
+        numbers.push_back(nbr);
+        return ;
+    }
+
     while (left <= right)
     {
         mid = left + (right - left) / 2;
@@ -113,6 +123,29 @@ void merge_insertion_with_vector(std::vector<unsigned int>& numbers, unsigned in
             left = mid + 1;
     }
     numbers.insert(left, nbr);
+}
+
+void insert_to_main_with_vector(std::vector<unsigned int>& main, std::vector<unsigned int>& pend)
+{
+    static bool first_time = true;
+    unsigned int step = 1;
+    
+    print_vector(pend, "Pend:");
+    if (first_time)
+    {
+        merge_insertion_with_vector(main, *pend.begin());
+        pend.erase(pend.begin());
+        step = 2;
+        first_time = false;
+    }
+    for (std::vector<unsigned int>::iterator it = pend.begin(); it < pend.end(); it += step)
+    {
+        merge_insertion_with_vector(main, *it);
+        pend.erase(it);
+    }
+    print_vector(pend, "Pend:");
+    if (pend.size())
+        insert_to_main_with_vector(main, pend);
 }
 
 int insert_into_vector(int argc, char **argv, std::vector<unsigned int>& numbers)
