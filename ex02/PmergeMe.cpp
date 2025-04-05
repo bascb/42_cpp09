8c2734e6630baddbf7cb26b49230847191c9311e
Bernardo Castelo Branco <bcastelo@c2r1s8student.42lisboa.com>
2025-04-05 17:55:24 +0100

    Vector merge insertion ready to final test

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -6,25 +6,13 @@
 /*   By: bcastelo <bcastelo@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 11:22:23 by bcastelo          #+#    #+#             */
-/*   Updated: 2025/04/05 11:47:07 by bcastelo         ###   ########.fr       */
+/*   Updated: 2025/04/05 17:49:39 by bcastelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
-void sort_pairs_with_vector(std::vector<unsigned int>::iterator start, std::vector<unsigned int>::iterator end, unsigned int set_size);
-
-unsigned int sort_all_pairs_with_vector(std::vector<unsigned int>::iterator start, std::vector<unsigned int>::iterator end);
-
 unsigned int binary_insertion_with_vector(std::vector<unsigned int>& numbers, unsigned int nbr);
-
-void populate_main_with_vector(std::vector<unsigned int>& main, std::vector<unsigned int>& pend, unsigned int to_main, unsigned int to_pend);
-
-void sort_larger_elements_with_vector(std::vector<unsigned int>& main, std::vector<unsigned int>& pend, std::vector<unsigned int>& original);
-
-void insert_into_main_with_vector(std::vector<unsigned int>& main, std::vector<unsigned int>& pend);
-
-void insert_to_main_with_vector(std::vector<unsigned int>& main, std::vector<unsigned int>& pend);
 
 bool push_number_to_vector(std::vector<unsigned int>& numbers, std::string number_str);
 
@@ -32,107 +20,36 @@ bool is_all_digits(std::string str);
 
 bool is_duplicated_in_vector(std::vector<unsigned int>& data, unsigned int value);
 
-//double get_time_microseconds(void);
-
-void print_diff_time(std::string step, struct timespec start, struct timespec end);
-
-std::vector<unsigned int> fordJohnsonSort(std::vector<unsigned int> arr);
-
-int merge_insertion_with_vector(std::vector<unsigned int>& original)
+ int merge_insertion_with_vector(std::vector<unsigned int>& original)
 {
     std::vector<unsigned int> main;
     std::vector<unsigned int> pend;
     std::vector<unsigned int>::iterator it;
-    static bool first_time = true;
     
-    main = original;
-    if (first_time)
-    {
-        sort_pairs_with_vector(main.begin(), main.end(), 1);
-        first_time = false;
-    }
-    if (!sort_all_pairs_with_vector(main.begin(), main.end()))
-        return (0);
-
-    /*if (original.size() <= 1)
+    if (original.size() <= 1)
         return (0);
     for (it = original.begin(); it + 1 < original.end(); it += 2)
     {
         if (*it > *(it + 1))
         {
-            //populate_main_with_vector(main, pend, *it, *(it + 1));
             main.push_back(*it);
             pend.push_back(*(it + 1));
         }
         else
         {
-            //populate_main_with_vector(main, pend,  *(it + 1), *it);
             main.push_back(*(it + 1));
             pend.push_back(*it);
         }
-    } */
+    }
     merge_insertion_with_vector(main);
-    first_time = true;
     if (original.size() % 2 == 1)
         pend.push_back(*(original.end() - 1));
-    main.insert(main.begin(), *(pend.begin()));
-    pend.erase(pend.begin());
-    //sort_pairs_with_vector(original.begin(), original.end(), 1);
-    //sort_larger_elements_with_vector(main, pend, original);
-    insert_into_main_with_vector(main, pend);
+    for (it = pend.begin(); it < pend.end(); ++it)
+    {
+        binary_insertion_with_vector(main, *it);
+    }
     original = main;
     return (0);
-}
-
-void sort_pairs_with_vector(std::vector<unsigned int>::iterator start, std::vector<unsigned int>::iterator end, unsigned int set_size)
-{
-    unsigned int aux;
-    unsigned int pair_size = set_size * 2;
-    std::vector<unsigned int>::iterator init = start;
-    
-    while (init + pair_size - 1 < end)
-    {
-        if (*(init + set_size - 1) > *(init + pair_size - 1))
-        {
-            for (std::vector<unsigned int>::iterator it = init; it < init + set_size; ++it)
-            {
-                aux = *it;
-                *it = *(it + set_size);
-                *(it + set_size) = aux;
-            }
-        }
-        init += pair_size;
-    }
-    //if (set_size < end - start)
-    //    sort_pairs_with_vector(start, end, set_size * 2);
-}
-
-unsigned int sort_all_pairs_with_vector(std::vector<unsigned int>::iterator start, std::vector<unsigned int>::iterator end)
-{
-    std::vector<unsigned int>::iterator exte;
-    std::vector<unsigned int>::iterator inte;
-    unsigned int aux;
-    unsigned int swap_nbr;
-    
-    swap_nbr = 0;
-    for (exte = start; exte < end - 1; exte += 2)
-    {
-        inte = exte + 2;
-        std::cout << "Before: " << *exte << " " << *inte << " ";
-        if (*(exte + 1) > *(inte + 1))
-        {
-            ++swap_nbr;
-            aux = *exte;
-            *exte = *inte;
-            *inte = aux;
-            aux = *(exte + 1);
-            *(exte + 1) = *(inte + 1);
-            *(inte + 1) = aux;
-        }
-        std::cout << "After: " << *exte << " " << *inte << std::endl;
-    }
-    std::cout << swap_nbr << std::endl;
-    return (swap_nbr);
 }
 
 unsigned int binary_insertion_with_vector(std::vector<unsigned int>& numbers, unsigned int nbr)
@@ -161,58 +78,6 @@ unsigned int binary_insertion_with_vector(std::vector<unsigned int>& numbers, un
     return (pos);
 }
 
-void populate_main_with_vector(std::vector<unsigned int>& main, std::vector<unsigned int>& pend, unsigned int to_main, unsigned int to_pend)
-{
-    unsigned int pos;
-
-    pos = binary_insertion_with_vector(main, to_main);
-    if (!pend.size() || pend.begin() + pos >= pend.end())
-        pend.push_back(to_pend);
-    else
-        pend.insert(pend.begin() + pos, to_pend);
-}
-
-void sort_larger_elements_with_vector(std::vector<unsigned int>& main, std::vector<unsigned int>& pend, std::vector<unsigned int>& original)
-{
-    std::vector<unsigned int>::iterator it;
-
-    for (it = original.begin() + 1; it < original.end(); it += 2)
-    {
-        populate_main_with_vector(main, pend, *it, *(it - 1));
-    }
-    if (original.size() % 2 ==1)
-        pend.push_back(*(it - 1));
-    main.insert(main.begin(), *(pend.begin()));
-    pend.erase(pend.begin());
-}
-
-void insert_into_main_with_vector(std::vector<unsigned int>& main, std::vector<unsigned int>& pend)
-{
-    std::vector<unsigned int>::iterator it;
-    std::vector<unsigned int>::iterator mid = pend.begin() + (pend.end() - 1 - pend.begin()) / 2;
-
-    for (it = pend.begin() + 1; it < mid; it += 2)
-    {
-        binary_insertion_with_vector(main, *it);
-        binary_insertion_with_vector(main, *(it - 1));
-    }
-    for (it = pend.end() - 1; it >= mid; --it)
-    {
-        binary_insertion_with_vector(main, *it);
-    }
-}
-
-void insert_to_main_with_vector(std::vector<unsigned int>& main, std::vector<unsigned int>& pend)
-{
-    for (std::vector<unsigned int>::iterator it = pend.begin(); it < pend.end(); ++it)
-    {
-        binary_insertion_with_vector(main, *it);
-        pend.erase(it);
-    }
-    if (pend.size())
-        insert_to_main_with_vector(main, pend);
-}
-
 int insert_into_vector(int argc, char **argv, std::vector<unsigned int>& numbers)
 {
     int counter = 0;
@@ -225,7 +90,6 @@ int insert_into_vector(int argc, char **argv, std::vector<unsigned int>& numbers
         while (iss >> token)
         {
             if (iss.fail() || !is_all_digits(token) || !push_number_to_vector(numbers, token))
-            if (!push_number_to_vector(numbers, argv[i]))
 	        {
 		        std::cerr << "Error: invalid input." << std::endl;
 		        return (0);
@@ -283,58 +147,4 @@ void print_original(int argc, char **argv)
         std::cout << argv[i] << " ";
     }
     std::cout << std::endl; 
-}
-
-double get_time_microseconds(void)
-{
-    struct timespec ts;
-    
-    clock_gettime(CLOCK_MONOTONIC, &ts);
-    return (ts.tv_sec * 1000000 + ts.tv_nsec / 1000.0);
-}
-
-//void print_diff_time(std::string step, struct timespec start, struct timespec end)
-//{
-//    std::cout << step << " : " << diff_in_microseconds(start, end) << std::endl;
-//}
-
-// Function to insert an element into a sorted sequence with minimal comparisons
-void mergeInsert(std::vector<unsigned int>& sorted, unsigned int element) {
-    int left = 0, right = sorted.size() - 1;
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (element < sorted[mid]) {
-            right = mid - 1;
-        } else {
-            left = mid + 1;
-        }
-    }
-    sorted.insert(sorted.begin() + left, element);
-}
-
-// Ford-Johnson sorting function
-std::vector<unsigned int> fordJohnsonSort(std::vector<unsigned int> arr) {
-    if (arr.size() <= 1) return arr;
-
-    std::vector<unsigned int> sorted;
-    
-    // Initial pairing and sorting
-    for (size_t i = 0; i + 1 < arr.size(); i += 2) {
-        unsigned int a = std::min(arr[i], arr[i + 1]);
-        unsigned int b = std::max(arr[i], arr[i + 1]);
-        if (sorted.empty() || b >= sorted.back()) {
-            sorted.push_back(a);
-            sorted.push_back(b);
-        } else {
-            mergeInsert(sorted, a);
-            mergeInsert(sorted, b);
-        }
-    }
-
-    // Handle the last unpaired element (if size is odd)
-    if (arr.size() % 2 == 1) {
-        mergeInsert(sorted, arr.back());
-    }
-
-    return sorted;
 }
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bcastelo <bcastelo@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 14:39:58 by bcastelo          #+#    #+#             */
-/*   Updated: 2025/03/31 19:38:51 by bcastelo         ###   ########.fr       */
+/*   Updated: 2025/04/05 17:53:35 by bcastelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ int	main(int argc, char **argv)
 {
     std::vector<unsigned int> sorted_vector;
     clock_t start, end;
+    double vector_time;
     
     if (argc < 2)
     {
@@ -30,15 +31,16 @@ int	main(int argc, char **argv)
         return (1);
     }
 
-    print_original(argc, argv);
     start = clock();
     if (!insert_into_vector(argc, argv, sorted_vector))
-        return (-1);
+        return (1);
     if (merge_insertion_with_vector(sorted_vector) == -1)
         return (1);
     end = clock();
-    std::cout << "Time to process a range of " << sorted_vector.size() << " elements with std::vector : " << diff_in_microseconds(start, end) << " us" << std::endl;
+    vector_time = diff_in_microseconds(start, end);
+    print_original(argc, argv);
     print_vector(sorted_vector, "After : ");
+    std::cout << "Time to process a range of " << sorted_vector.size() << " elements with std::vector : " << vector_time << " us" << std::endl;
     
     return (0);
 }
